fda8b1997779d02573daa129243087f1b851f9c8
victoriamelo16 <mvictoriamelo11@outlook.com>
2023-11-24 21:50:13 -0300

    Eventos adicionados
    Eventos e funções foram adicionados à página de cadastro

---
+++ js/scriptcad.js
@@ -0,0 +1,13 @@
+function emDestaque() {
+   document.getElementById("senha02").style.borderColor = '#DDA0DD'
+}
+function semDestaque() {
+   document.getElementById("senha02").style.borderColor = '#7F7F7F'
+}
+function aoPassar() {
+   document.getElementById("btnCad").style.backgroundColor = "#DDA0DD";
+}
+function aoSair() {
+   document.getElementById("btnCad").style.backgroundColor = "#eac2ea";
+}
+    
