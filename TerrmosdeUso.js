9bf758eff2c0d2f835a52a95ee5094995afaaa77
Daniss06 <daniss.euc23@gmail.com>
2024-02-21 22:55:57 -0300

    CSS INSERIDO
    Css inserido para designer do footer e dos botões de alterações de conteúdo da página;
    Feito melhorias na formatação de texto.

--- TerrmosdeUso.js
+++ TerrmosdeUso.js
@@ -1,15 +1,16 @@
 // Função para mostrar os Termos de Uso e esconder a Política de Privacidade
 function showTerms() {
+    document.getElementById('termsContent').classList.add('hidden');
     document.getElementById('termsContent').classList.remove('hidden');
-    document.getElementById('privacyContent').classList.add('hidden');
 }
 
 // Função para mostrar a Política de Privacidade e esconder os Termos de Uso
 function showPrivacyPolicy() {
-    document.getElementById('privacyContent').classList.remove('hidden');
     document.getElementById('termsContent').classList.add('hidden');
+    document.getElementById('privacyContent').classList.remove('hidden');
 }
 
 // Associando as funções aos botões
 document.getElementById("showTermsButton").addEventListener("click", showTerms);
 document.getElementById("showPrivacyButton").addEventListener("click", showPrivacyPolicy);
+
